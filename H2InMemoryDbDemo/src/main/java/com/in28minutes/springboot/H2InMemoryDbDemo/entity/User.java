ca2005e28a1fa5b270570af0c9ece775e757415c
kunal.s <kunals004@yahoo.co.in>
2018-08-08 17:11:53 +0530

    Updated entity and service

--- H2InMemoryDbDemo/src/main/java/com/in28minutes/springboot/H2InMemoryDbDemo/entity/User.java
+++ H2InMemoryDbDemo/src/main/java/com/in28minutes/springboot/H2InMemoryDbDemo/entity/User.java
@@ -7,20 +7,45 @@ import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 
 @Entity
-@NamedQuery (query = "select u from User u", name = "query_find_all_users")
+@NamedQuery(query = "select u from User u", name = "query_find_all_users")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
 	private String name;
 	private String role;
-	
-	protected User() {	
+
+	protected User() {
 	}
-	
+
 	public User(String name, String role) {
 		this.name = name;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
 		this.role = role;
 	}
 
