269b7e2a981353a64b412fbe7cf8262300d570da
kunal.s <kunals004@yahoo.co.in>
2018-08-09 14:53:45 +0530

    Added controller and property values

--- hello-boot/src/main/java/com/example/demo/HelloController.java
+++ hello-boot/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	@Autowired
+	HelloProperties props;
+	
 	@RequestMapping("/hello")
 	public String hello(@RequestParam String name) {
-		return "hhhhhhhheeeeeeeeeeelllllllllllloooooooo!   "+name;
+		return props.getGreeting()+name;
 	}
 
 }
